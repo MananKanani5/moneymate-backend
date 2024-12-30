41f514c30153a9717f7de0225cb0b063a52af836
MananKanani5 <kanani.manan5@gmail.com>
2024-12-30 21:47:57 +0530

    session options fixed

--- app.js
+++ app.js
@@ -38,9 +38,8 @@ app.use(
     cookie: {
       maxAge: 1000 * 60 * 60 * 24,
       httpOnly: true,
-      secure: false,
-      sameSite: "None",
       secure: process.env.NODE_ENV === "production",
+      sameSite: "None",
     },
   })
 );
